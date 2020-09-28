dd0fbce267a736608a9f93e8fea42354ecb0e894
Codegem <evaldaskis@gmail.com>
2020-09-28 12:14:46 +0300

    Start of BLog

---
+++ resources/views/naujas.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Naujas</title>
+</head>
+<body>
+    <h1>Sveikas cia naujas puslapis</h1>
+    <h2>Is Laravel</h2>
+</body>
+</html>
